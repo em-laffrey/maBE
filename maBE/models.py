8ac056e51be9405793bba8ec05f500e70d9bece9
Emily <e.laffrey@paper.co>
2025-03-07 12:54:20 -0600

    add be

---
+++ maBE/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=30)
+    address = models.CharField(max_length=30, null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    zip_code = models.CharField(max_length=30, null=True, blank=True)
+    country = models.CharField(max_length=30, null=True, blank=True)
+    company_name = models.CharField(max_length=30, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
